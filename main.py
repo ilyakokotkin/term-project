463cf9214f2b2f706d572ba3a0ee20ec6c6d9682
ilyakokotkin <ko.ilya@live.com>
2023-12-14 05:41:21 +0000

    Added: 1. Saves changes to the input file 2. Added error handling 3. Now if error when removing an investment, the program asks the user for input again

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
+"""
+This is the main module for the Investment Portfolio Management Program.
+It allows users to manage their investment portfolio through 
+a command-line interface.
+"""
+
 from portfolio import InvestmentPortfolio
 
 def read_portfolio_data(file_name):
+    """
+    Reads investment data from a file.
+
+    Args:
+        file_name (str): The name of the file to read investment data from.
+
+    Returns:
+        dict: A dictionary of investments.
+    """
     investments = {}
     try:
         with open(file_name, 'r') as file:
@@ -8,22 +23,76 @@ def read_portfolio_data(file_name):
                 name, amount = line.split(',')
                 investments[name.strip()] = float(amount.strip())
     except FileNotFoundError:
-        print("File not found.")
+        print("File not found. Please check the file's existence or path.")
     else:
         return investments
+    
+def get_user_input(prompt, data_type=float):
+    """
+    Gets user input and validates it.
 
-def main():
+    Args:
+        prompt (str): The prompt to display to the user.
+        data_type (type, optional): The type of data expected.
+
+    Returns:
+        The validated user input converted to the specified data type.
+    """
+    while True:
+        try:
+            return data_type(input(prompt))
+        except ValueError:
+            print(f"Please enter a valid {data_type.__name__}.")
+
+def manage_portfolio(portfolio):
+    """
+    Manages the investment portfolio through user input by
+    caling InvestmentPortfolio class methods.
+
+    Args:
+        portfolio (InvestmentPortfolio): The investment portfolio to manage.
+    """
+    while True:
+        print("\n1. Add Investment\n2. Remove Investment\n3. View Portfolio\n4. Exit")
+        choice = input("Choose an option: ")
+
+        # Add investment
+        if choice == '1':
+            name = input("Enter investment name: ")
+            amount = get_user_input("Enter amount (e.g., 5000.00): ")
+            portfolio.add_investment(name, amount)
+            print(f"Investment '{name}' added.")
+
+        # Remove investment
+        elif choice == '2':
+            while True:
+                try:
+                    name = input("Enter investment name to remove: ")
+                    portfolio.remove_investment(name)
+                    print(f"Investment '{name}' removed.")
+                    break
+                except ValueError as e:
+                    print(e)
+
+        # View portfolio
+        elif choice == '3':
+            print(portfolio)
+
+        # Save changes in input file and exit the program
+        elif choice == '4':
+            portfolio.save_to_file('investments.txt') 
+            print("Portfolio changes saved. Exiting program.")
+            break
+        else:
+            print("Invalid option. Please try again.")
+
+if __name__ == "__main__":
     portfolio_data_file = 'investments.txt'
     investments_data = read_portfolio_data(portfolio_data_file)
 
+    portfolio = InvestmentPortfolio("My Portfolio")
     if investments_data:
-        portfolio = InvestmentPortfolio("My Portfolio")
-        
         for name, amount in investments_data.items():
             portfolio.add_investment(name, amount)
 
-        print(portfolio)
-        print("Apple Investment Details:", portfolio['Apple'])  # Using __getitem__
-
-if __name__ == "__main__":
-    main()
+    manage_portfolio(portfolio)
--- portfolio.py
+++ portfolio.py
@@ -1,23 +1,105 @@
+"""
+This module defines the InvestmentPortfolio class, 
+which manages investment operations for the main program.
+"""
 class InvestmentPortfolio:
+    """
+    A class to represent an investment portfolio.
+
+    Attributes:
+        portfolio_name (str): The name of the portfolio.
+        investments (dict): A dictionary to store investment details.
+        _balance (float): A private attribute to track the 
+        total investment balance.
+    """
     def __init__(self, name):
+        """
+        Constructs all the necessary attributes for the 
+        InvestmentPortfolio object.
+
+        Args:
+            name (str): The name of the investment portfolio.
+        Returns:
+            nothing
+        """
         self.portfolio_name = name
         self.investments = {}
         self._balance = 0
 
     def __str__(self):
-        return f"Portfolio '{self.portfolio_name}' with balance: {self._balance}"
+        """
+        Returns a string representation of the investment portfolio,
+        including each stock and its value, and the total balance.
+        """
+        portfolio_details = [(f"-" * 20)]
+        portfolio_details.append(f"Portfolio: '{self.portfolio_name}' \nBreakdown:")
+        for stock, value in self.investments.items():
+            portfolio_details.append(f"  - {stock}: {value:,.2f}")
+        portfolio_details.append((f"-" * 20))
+        portfolio_details.append(f"Total balance: {self._balance:,.2f}")
+        return "\n".join(portfolio_details)
 
     def __getitem__(self, item):
+        """
+        Allows dictionary-like access to the investments.
+
+        Args:
+            item (str): The name of the investment.
+
+        Returns:
+            The amount of the specified investment or a not found message.
+        """
         return self.investments.get(item, "Not found")
 
     def _update_balance(self):
+        """
+        A private method to update the portfolio balance based on the investments.
+        
+        Args:
+            none
+        Returns:
+            nothing
+        """
+
         self._balance = sum(self.investments.values())
 
     def add_investment(self, name, amount):
+        """
+        Adds a new investment to the portfolio.
+
+        Args:
+            name (str): The name of the investment.
+            amount (float): The amount to be invested.
+        Returns:
+            nothing
+        """
         self.investments[name] = amount
         self._update_balance()
 
     def remove_investment(self, name):
+        """
+        Removes an investment from the portfolio.
+
+        Args:
+            name (str): The name of the investment to be removed.
+        Returns:
+            nothing
+        Raises:
+            ValueError: If the investment name is not found in the portfolio.
+        """
         if name in self.investments:
             del self.investments[name]
             self._update_balance()
+        else:
+            raise ValueError(f"InvalidInput: '{name}' investment name is not found")
+    
+    def save_to_file(self, file_name):
+        """
+        Saves the current state of the investment portfolio to the input file.
+
+        Args:
+            file_name (str): The name of the file to save the portfolio data.
+        """
+        with open(file_name, 'w') as file:
+            for name, amount in self.investments.items():
+                file.write(f"{name},{amount}\n")
